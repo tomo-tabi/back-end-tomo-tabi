f568a8896693b85e13a87cea6a58f8a468a1cc71
raeki <mkeithley@gmail.com>
2022-12-21 06:11:30 -0500

    build: set default server port to 8080

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const db = require('./db/knex');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
