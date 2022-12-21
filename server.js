7026de40795d15d9f35991a12fe6e57b07ef78cf
raeki <mkeithley@gmail.com>
2022-12-21 06:55:50 -0500

    feat: initial server routing

--- server.js
+++ server.js
@@ -5,9 +5,27 @@ const cors = require('cors');
 const timeout = require('connect-timeout');
 const port = process.env.PORT || 8080;
 
+// Import route files
+const userRoutes = require('./routes/users/userRoutes');
+const tripRoutes = require('./routes/trips/tripRoutes');
+const timelineRoutes = require('./routes/timeline/timelineRoutes');
+const expenseRoutes = require('./routes/expenses/expenseRoutes');
+
+// Initialize server utilities
 app.use(cors());
 app.use(express.json());
 app.use(timeout('5s'));
+
+// Initial server routes
+app.use('/user', userRoutes);
+app.use('/trip', tripRoutes);
+app.use('/timeline', timelineRoutes);
+app.use('/timeline', expenseRoutes);
+
+// function to check if the request has timed out
+function haltOnTimedout(req, res, next) {
+  if (!req.timedout) next();
+}
 
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
