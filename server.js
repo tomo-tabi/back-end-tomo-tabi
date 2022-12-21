807677fafd4814cc5df2085fd10b948217c88db0
raeki <mkeithley@gmail.com>
2022-12-21 06:17:18 -0500

    build: add timeout to server

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const db = require('./db/knex');
+const timeout = require('connect-timeout');
 const port = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
+app.use(timeout('5s'));
 
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
