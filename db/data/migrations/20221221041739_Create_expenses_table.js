503b7093e654f76f0a64f326ab4cdd069c578e29
Pol Texido <107403548+calss0t@users.noreply.github.com>
2022-12-24 05:43:20 +0900

    Created trip and some expenses routes. Need to check the routes and how the frontend wants to recieve and send the information. We can change the way they send with body or as a param.

--- db/data/migrations/20221221041739_Create_expenses_table.js
+++ db/data/migrations/20221221041739_Create_expenses_table.js
@@ -2,15 +2,21 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function (knex) {
-  return knex.schema.createTable('expenses', function (table) {
-    table.increments('id').primary();
-    table.integer('user_id').notNullable();
-    table.foreign('user_id').references('users.id');
-    table.string('item_name', 255).notNullable();
-    table.string('username', 255).notNullable();
-    table.decimal('money').notNullable();
-  });
+
+
+//We should create a new migartion and update this table to have a trip id column
+//In the temp file I wrote the code as if we already had this value with the name trip_id
+
+
+exports.up = function(knex) {
+    return knex.schema.createTable("expenses", function (table) {
+		table.increments("id").primary();
+		table.integer("user_id").notNullable()
+        table.foreign("user_id").references("users.id")
+        table.string("item_name", 255).notNullable()
+        table.string("username", 255).notNullable()
+        table.decimal("money").notNullable()
+	});
 };
 
 /**
