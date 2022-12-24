cac25b01ad1f84108ab7e59711ee932342ebdbf5
raeki <mkeithley@gmail.com>
2022-12-23 22:11:44 -0500

    chore: run linter

--- db/data/migrations/20221224021920_Create_tripsEvents_table.js
+++ db/data/migrations/20221224021920_Create_tripsEvents_table.js
@@ -2,20 +2,20 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-    return knex.schema.createTable("tripsEvents", function (table) {
-		table.increments("id").primary();
-		table.integer("trips_id").notNullable()
-        table.foreign("trips_id").references("trips.id")
-        table.string("event_name", 255).notNullable()
-        table.date("event_date").notNullable()
-	});
+exports.up = function (knex) {
+  return knex.schema.createTable('tripsEvents', function (table) {
+    table.increments('id').primary();
+    table.integer('trips_id').notNullable();
+    table.foreign('trips_id').references('trips.id');
+    table.string('event_name', 255).notNullable();
+    table.date('event_date').notNullable();
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-    return knex.schema.dropTable('tripsEvents');
+exports.down = function (knex) {
+  return knex.schema.dropTable('tripsEvents');
 };
