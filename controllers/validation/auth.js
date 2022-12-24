647f3a17ba6a0b927ba3ead3cbaf2ae17f1dcfa9
raeki <mkeithley@gmail.com>
2022-12-24 16:14:12 -0500

    feat: refactor token not found logic

--- controllers/validation/auth.js
+++ controllers/validation/auth.js
@@ -1,27 +1,32 @@
 // JWT verification
-require("dotenv").config({ path: "../.env" });
-const jwt = require("jsonwebtoken");
+require('dotenv').config({ path: '../.env' });
+const jwt = require('jsonwebtoken');
 
 // CREATE TOKEN
 
-const createTokens = (user) => {
-    const accessToken = sign({ username: user.username, id: user.id }, process.env.ACCESS_TOKEN_SECRET)
-    res.json({ accessToken: accessToken })
-}
+const createTokens = user => {
+  const accessToken = sign(
+    { username: user.username, userid: user.id },
+    process.env.ACCESS_TOKEN_SECRET
+  );
+  res.json({ accessToken: accessToken });
+};
 
 // AUTHENTICATE TOKEN
 
 const authenticateToken = (req, res, next) => {
-    const header = req.headers['authorization']
-    // if header exist return token portion, otherwise return undefined
-    const token = header && header.split(' ')[1]
-    if (token == null) return res.sendStatus(401)
+  const token = req.headers.authorization.split(' ')[1];
 
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if (err) return res.sendStatus(403)
-        req.user = user
-        next()
-    })
-}
+  // if token is not present user is not authorized
+  if (!token) {
+    return res.status(401).send('token not found');
+  }
 
-module.exports = { createTokens, authenticateToken }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+};
+
+module.exports = { createTokens, authenticateToken };
