de55b30e063f7f6dd2d5334f48eb89e1635a3449
raeki <mkeithley@gmail.com>
2022-12-26 19:41:16 -0500

    feat: validate email format and non-existence

--- routes/users/userRoutes.js
+++ routes/users/userRoutes.js
@@ -1,17 +1,28 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../../controllers/users/userController');
+const validate = require('../../middleware/validate');
 const { authenticateToken } = require('../../middleware/auth');
 
 // routes that don't require jwt authentication
-router.post('/login', userController.login);
-router.post('/signup', userController.signup);
+router.post('/login', validate.emailFormat, userController.login);
+router.post(
+  '/signup',
+  validate.emailFormat,
+  validate.emailNotExists,
+  userController.signup
+);
 
 // require jwt authentication for all subsequent requests
 router.use(authenticateToken);
 
 router.get('/', userController.getUser);
-router.put('/update', userController.putUser);
+router.put(
+  '/update',
+  validate.emailFormat,
+  validate.emailNotExists,
+  userController.putUser
+);
 router.delete('/delete', userController.deleteUser);
 
 module.exports = router;
