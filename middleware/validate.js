58c576422b650bb679b64a458309c1e4a2d6a0f0
Matthew K <mkeithley@gmail.com>
2023-01-16 13:49:52 +0900

    refactor: use json

--- middleware/validate.js
+++ middleware/validate.js
@@ -14,7 +14,7 @@ const emailFormat = function (req, res, next) {
   const { email } = req.body;
 
   // Check for a valid email address
-  if (!email.match(REGEX.EMAIL)) return res.status(400).send('Invalid email');
+  if (!email.match(REGEX.EMAIL)) return res.status(400).json({ message: 'Invalid email' });
 
   // else move on to next function
   next();
@@ -35,13 +35,13 @@ const emailNotExists = async function (req, res, next) {
   try {
     // Check if the email already exists in the database
     const data = await knex.select('email').from('users').where('email', email);
-    if (data.length) return res.status(409).send('email already exists');
+    if (data.length) return res.status(409).json({ message: 'email already exists' });
 
     // else move on to the next function
     next();
   } catch (e) {
     console.log(e);
-    return res.status(500).send('Internal Server Error');
+    return res.status(500).json({ message: 'Internal Server Error' });
   }
 };
 
@@ -58,13 +58,13 @@ const userToTrip = async function (req, res, next) {
       .where({ user_id: userid, trip_id: tripid });
 
     // if no connection response with status code 403
-    if (!exists.length) return res.sendStatus(403);
+    if (!exists.length) return res.status(403).json({ message: 'user not authorized or this trip' });
 
     // all is well
     next();
   } catch (e) {
     console.log(e);
-    res.sendStatus(500);
+    return res.status(500).json({ message: 'Internal Server Error' });
   }
 };
 
