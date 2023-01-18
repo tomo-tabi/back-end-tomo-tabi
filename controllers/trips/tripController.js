20b3906d282f5536b5b1d16edd66cb8701968c1a
Raeki <mkeithley@gmail.com>
2023-01-18 21:09:58 +0900

    fix: return res.send

--- controllers/trips/tripController.js
+++ controllers/trips/tripController.js
@@ -58,7 +58,7 @@ const getTripUsers = async function (req, res) {
     if (!data.length) return res.status(404).json({ message: 'not found' });
 
     // send data
-    res.status(200).json(data);
+    return res.status(200).json(data);
   } catch (error) {
     console.log(error);
     return res.status(500).json({ message: 'Internal server error' });
