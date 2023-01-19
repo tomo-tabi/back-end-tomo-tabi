fe21d3bb3cb4bb7b94add743cde7028adb144efe
Raeki <mkeithley@gmail.com>
2023-01-19 21:07:07 +0900

    eslint: inviteController

--- controllers/invites/inviteController.js
+++ controllers/invites/inviteController.js
@@ -10,7 +10,7 @@ const { getIdFromEmail } = require('../../utils/getID');
  * @returns {Response} returns an array of invite objects where status is pending
  */
 
-const getInvites = async function (req, res) {
+async function getInvites(req, res) {
   try {
     // extract the userid from req.body
     const { userid } = req.body;
@@ -26,15 +26,18 @@ const getInvites = async function (req, res) {
       .where({ receiver_id: userid, status: PENDING });
 
     // check that info exists
-    if (!data.length) { return res.status(404).json({ message: 'no invites found' }); }
+    if (!data.length) {
+      return res.status(404).json({ message: 'no invites found' });
+    }
 
     // send the data
     return res.status(200).json(data);
   } catch (error) {
-    console.log(error);
+    // eslint-disable-next-line no-console
+    console.error(error);
     return res.status(500).json({ message: 'Internal Server Error' });
   }
-};
+}
 
 /**
  * Respond to a POST request to API_URL/invite/create
@@ -43,7 +46,7 @@ const getInvites = async function (req, res) {
  * @returns {Response} returns a CREATED status
  */
 
-const createInvite = async function (req, res) {
+async function createInvite(req, res) {
   try {
     // extract required information from req.body
     const { userid, tripid, email } = req.body;
@@ -51,7 +54,9 @@ const createInvite = async function (req, res) {
     const receiverid = await getIdFromEmail(email);
 
     // confirm all required information is defined
-    if (!userid || !receiverid || !tripid) { return res.status(500).json('required variable is undefined'); }
+    if (!userid || !receiverid || !tripid) {
+      return res.status(500).json('required variable is undefined');
+    }
 
     const data = await knex('invites').insert(
       {
@@ -60,19 +65,22 @@ const createInvite = async function (req, res) {
         trip_id: tripid,
         status: PENDING,
       },
-      'id',
+      'id'
     );
 
     // confirm the new data has been saved in data
-    if (!data.length) { return res.status(500).json({ message: 'Internal Server Error' }); }
+    if (!data.length) {
+      return res.status(500).json({ message: 'Internal Server Error' });
+    }
 
     // send status code 'CREATED'
     return res.status(201).json({ message: 'invite created' });
   } catch (error) {
-    console.log(error);
+    // eslint-disable-next-line no-console
+    console.error(error);
     return res.status(500).json({ message: 'Internal Server Error' });
   }
-};
+}
 
 /**
  * Respond to a PUT request to API_URL/invite/accept/:inviteid
@@ -81,16 +89,18 @@ const createInvite = async function (req, res) {
  * @returns {Response} returns an http response 200
  */
 
-const acceptInvite = async function (req, res) {
+async function acceptInvite(req, res) {
   try {
     // extract all required information from req.body
     const { inviteid } = req.params;
     const { userid } = req.body;
 
     // confirm all required information is defined
-    if (!inviteid || !userid) { return res.status(500).json('required variable is undefined'); }
+    if (!inviteid || !userid) {
+      return res.status(500).json('required variable is undefined');
+    }
 
-    await knex.transaction(async (trx) => {
+    await knex.transaction(async trx => {
       // update invite to accepted using inviteid
       const tripid = await knex('invites')
         .where('id', inviteid)
@@ -105,10 +115,11 @@ const acceptInvite = async function (req, res) {
 
     return res.status(200).json({ message: 'invite accepted' });
   } catch (error) {
-    console.log(error);
+    // eslint-disable-next-line no-console
+    console.error(error);
     return res.status(500).json({ message: 'Internal Server Error' });
   }
-};
+}
 
 /**
  * Respond to a PUT request to API_URL/invite/reject/:tripid
@@ -117,7 +128,7 @@ const acceptInvite = async function (req, res) {
  * @returns {Response} returns an http response 200
  */
 
-const rejectInvite = async function (req, res) {
+async function rejectInvite(req, res) {
   try {
     // extract all required information from req.body
     const { inviteid } = req.params;
@@ -136,10 +147,11 @@ const rejectInvite = async function (req, res) {
 
     return res.status(200).json({ message: 'invite rejected' });
   } catch (error) {
-    console.log(error);
+    // eslint-disable-next-line no-console
+    console.error(error);
     return res.status(500).json({ message: 'Internal Server Error' });
   }
-};
+}
 
 /**
  * Respond to a DELETE request to API_URL/invite/:inviteid with status code 200
@@ -148,7 +160,7 @@ const rejectInvite = async function (req, res) {
  * @returns {Response} returns an http status code 200
  */
 
-const deleteInvite = async function (req, res) {
+async function deleteInvite(req, res) {
   try {
     // extract info from req.body and req.params
     const { inviteid } = req.params;
@@ -168,10 +180,11 @@ const deleteInvite = async function (req, res) {
 
     return res.status(200).json({ message: 'item deleted' });
   } catch (error) {
-    console.log(error);
+    // eslint-disable-next-line no-console
+    console.error(error);
     return res.status(500).json({ message: 'Internal Server Error' });
   }
-};
+}
 
 module.exports = {
   getInvites,
