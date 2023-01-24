8e36e184ae0a2b5ed72dc1111ca04ed7949e8233
Matthew K <mkeithley@gmail.com>
2023-01-24 01:47:42 +0000

    fix: knex config?

--- db/knex.js
+++ db/knex.js
@@ -3,4 +3,6 @@ const knex = require('knex');
 const knexConfig = require('./knexfile');
 const NODE_ENV = process.env.NODE_ENV;
 
+console.log(knexConfig.NODE_ENV);
+
 module.exports = knex(knexConfig.NODE_ENV);
--- db/knexfile.js
+++ db/knexfile.js
@@ -18,7 +18,7 @@ module.exports = {
     },
   },
   production: {
-    client: 'postgresql',
+    client: 'pg',
     connection: process.env.DATABASE_URL,
     migrations: {
       directory: './data/migrations',
