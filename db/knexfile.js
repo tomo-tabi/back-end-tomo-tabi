78bb94c906995a40d15d29bacbc4f032b2f9d124
Matthew K <mkeithley@gmail.com>
2023-01-24 01:57:46 +0000

    fix: knexconfig

--- db/knexfile.js
+++ db/knexfile.js
@@ -18,8 +18,10 @@ module.exports = {
     },
   },
   production: {
-    client: 'pg',
     connection: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
     migrations: {
       directory: './data/migrations',
     },
