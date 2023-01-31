79cdb328f27f9d8da221c73532d27f302916ef83
Raeki <mkeithley@gmail.com>
2023-01-31 19:54:22 +0900

    db: add is_locked boolean to trips table

---
+++ db/data/migrations/20230131105120_trips_add_is_locked.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.alterTable('trips', table => {
+    table.boolean('is_locked').defaultTo(false);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.alterTable('trips', table => {
+    table.dropColumn('is_locked');
+  });
+};
