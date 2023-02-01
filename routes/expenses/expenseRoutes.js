47fbbdffe130e22b285de6aaa0318b1e96ccf17c
Raeki <mkeithley@gmail.com>
2023-02-01 21:33:58 +0900

    feat: add exitOnLocked to routes

--- routes/expenses/expenseRoutes.js
+++ routes/expenses/expenseRoutes.js
@@ -4,13 +4,19 @@ const router = express.Router();
 const expenseCtrl = require('../../controllers/expenses/expenseController');
 const { authenticateToken } = require('../../middleware/auth');
 const { userToTrip } = require('../../middleware/validate');
+const { exitOnLocked } = require('../../middleware/permissions');
 
 router.use(authenticateToken);
 
 router.get('/:tripid', userToTrip, expenseCtrl.getExpenses);
-router.post('/create', userToTrip, expenseCtrl.createExpense);
-router.put('/update/:expenseid', expenseCtrl.updateExpense);
-router.delete('/delete/:expenseid', userToTrip, expenseCtrl.deleteExpense);
+router.post('/create', userToTrip, exitOnLocked, expenseCtrl.createExpense);
+router.put('/update/:expenseid', exitOnLocked, expenseCtrl.updateExpense);
+router.delete(
+  '/delete/:expenseid',
+  userToTrip,
+  exitOnLocked,
+  expenseCtrl.deleteExpense
+);
 router.get('/average/:tripid', userToTrip, expenseCtrl.getAverageExpense);
 
 module.exports = router;
