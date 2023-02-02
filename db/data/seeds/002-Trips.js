80d7f3a63e123c1c8ba96a81c4ba7a9afb97f4ce
Calss0t <pol.texido.b@gmail.com>
2023-02-02 20:47:31 +0900

    Added trips table info

--- db/data/seeds/002-Trips.js
+++ db/data/seeds/002-Trips.js
@@ -7,16 +7,16 @@ exports.seed = async function (knex) {
   await knex('trips').del();
   await knex('trips').insert([
     {
-      id: 1, start_date: '2023-01-01', end_date: '2023-01-12', name: 'Japan',
+      id: 1, start_date: '2023-01-01', end_date: '2023-01-12', name: 'Japan', is_locked: true, owner_id:1
     },
     {
-      id: 2, start_date: '2023-01-26', end_date: '2023-02-09', name: 'Boston',
+      id: 2, start_date: '2023-01-26', end_date: '2023-02-09', name: 'Boston', is_locked: false, owner_id:2
     },
     {
-      id: 3, start_date: '2023-03-05', end_date: '2023-03-15', name: 'South Korea',
+      id: 3, start_date: '2023-03-05', end_date: '2023-03-15', name: 'South Korea', is_locked: false, owner_id:3
     },
     {
-      id: 4, start_date: '2022-12-21', end_date: '2022-12-31', name: 'Barcelona',
+      id: 4, start_date: '2022-12-21', end_date: '2022-12-31', name: 'Barcelona', is_locked: true, owner_id:4
     },
   ]);
 };
